044b1cc194a041629eb3240c609e9fb7630d4655
your-lithium <lithium@knu.ua>
2025-02-13 21:13:36 +0200

    Add sliders for choosing game parameters

--- app/routers/main.py
+++ app/routers/main.py
@@ -13,15 +13,24 @@ async def index(request: Request):
 
 
 @main_router.get("/create")
-async def create(request: Request, mode: str):
-    if mode == "Small: 9×9, 10 mines":
-        rows, columns, mines = 9, 9, 10
-    elif mode == "Medium: 16×16, 40 mines":
-        rows, columns, mines = 16, 16, 40
-    elif mode == "Hard: 30×16, 99 mines":
-        rows, columns, mines = 30, 16, 99
-    elif mode == "Extreme: 24×30, 160 mines":
-        rows, columns, mines = 24, 30, 160
+async def create(
+    request: Request,
+    mode: str | None = None,
+    rows: int | None = None,
+    columns: int | None = None,
+    mines: int | None = None,
+):
+    if mode:
+        if mode == "Small: 9×9, 10 mines":
+            rows, columns, mines = 9, 9, 10
+        elif mode == "Medium: 16×16, 40 mines":
+            rows, columns, mines = 16, 16, 40
+        elif mode == "Hard: 30×16, 99 mines":
+            rows, columns, mines = 30, 16, 99
+        elif mode == "Extreme: 24×30, 160 mines":
+            rows, columns, mines = 24, 30, 160
+    elif not (rows and columns and mines):
+        raise ValueError("There must be either mode or rows, columns and mines specified")
     return jinja2_templates.TemplateResponse(
         "game.html", context={"request": request, "rows": rows, "columns": columns, "mines": mines}
     )
---
+++ app/static/scripts_index.js
@@ -0,0 +1,19 @@
+document.getElementById('height').addEventListener('input', function() {
+    document.getElementById('heightNumber').value = this.value;
+});
+document.getElementById('width').addEventListener('input', function() {
+    document.getElementById('widthNumber').value = this.value;
+});
+document.getElementById('mines').addEventListener('input', function() {
+    document.getElementById('minesNumber').value = this.value;
+});
+
+document.getElementById('heightNumber').addEventListener('input', function() {
+    document.getElementById('height').value = this.value;
+});
+document.getElementById('widthNumber').addEventListener('input', function() {
+    document.getElementById('width').value = this.value;
+});
+document.getElementById('minesNumber').addEventListener('input', function() {
+    document.getElementById('mines').value = this.value;
+});
